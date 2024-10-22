40a23bd60057e474ce48728ae2d022af06ad0802
9Nadi_ <marrief19@gmail.com>
2024-10-22 15:57:47 +0700

    Password generator, jam: 10:30, kelas: B

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // TODO: Implement the password generation logic based on user input
 
-const generatePassword = (length, options) => {
-    // Character sets for password generation
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const specialChars = "!@#$%^&*()";
+export const generatePassword = (length, options) => {
+  // Character sets for password generation
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const specialChars = "!@#$%^&*()";
 
-    // TODO: Create a variable for the character set based on selected options
+  // Create a variable for the character set based on selected options
+  let charSheet = "";
 
-    // TODO: Generate the password based on the selected criteria
-    return password;
+  if (options.includeUppercase) charSheet += uppercase;
+  if (options.includeLowercase) charSheet += lowercase;
+  if (options.includeNumbers) charSheet += numbers;
+  if (options.includeSpecialChars) charSheet += specialChars;
+
+
+  if (charSheet === "") {
+    throw new Error("At least one character type must be selected.");
+  }
+
+  // Generate the password based on the selected criteria
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charSheet.length);
+    password += charSheet[randomIndex];
+  }
+  return password;
 };
 
-// TODO: Add event listener to the button to call generatePassword and display the output
-
-// BONUS: Implement the copy to clipboard functionality
